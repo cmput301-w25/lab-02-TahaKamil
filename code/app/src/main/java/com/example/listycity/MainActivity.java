c24d4e256dca12cbfba9f17f974a06dcb15571dc
“Taha <tkamil@ualberta.ca>
2025-01-16 20:19:24 -0700

    Implmented add and remove methods

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +20,8 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     ListView cityList;
+    Button addButton;
+    EditText addCity;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
@@ -40,5 +46,25 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.context, dataList);
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                dataList.remove(i);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+        addButton = findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCity = findViewById(R.id.add_city);
+
+                dataList.add(addCity.getText().toString());
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
